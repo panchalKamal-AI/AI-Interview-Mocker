4efe870615dd4b7df04aa1ce000cef192922e828
panchalKamal-AI <panchkamal26@gmail.com>
2025-07-15 15:26:33 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:npg_D6EBSNmMr5Hs@ep-red-sound-a8jyq1om-pooler.eastus2.azure.neon.tech/neondb?sslmode=require',
+      url: 'enter your drizzle url here',
     }
   };
